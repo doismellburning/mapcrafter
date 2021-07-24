4a8e2fceecb41f7b5a215e0c8c2b85e0cd4b5f36
Dinip <8941012+Dinip@users.noreply.github.com>
2021-07-24 22:32:45 +0100

    Markers having issues on 1.17, removed temporarily

--- render.sh
+++ render.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 #Renders the markers and the map
-/usr/local/bin/mapcrafter_markers -c /config/render.conf >> /config/mapcrafter.log 2>&1 && \
+# Markers having issues on 1.17, removed temporarily
+# /usr/local/bin/mapcrafter_markers -c /config/render.conf >> /config/mapcrafter.log 2>&1 && \
 /usr/local/bin/mapcrafter -j $threads -c /config/render.conf >> /config/mapcrafter.log 2>&1 && \
 
 #Just to make sure that title and favicon are kept after the render is finished
